5ff0cf6bffc52cd6b3206530cf5e942b2838a19a
logon84 <logon84@users.noreply.github.com>
2023-04-02 00:34:59 +0200

    Add files via upload

--- ble_log_parser.sh
+++ ble_log_parser.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
-tshark -Y btatt -O hci_h4,btatt -r $1 | awk -F'[: ]+' '$1~/Frame/ { printf "\n%s ", $2}; $2~/Direct/ {printf "%s ", $3}; $2~/Value/ {printf "%s", $3}'
+
+tshark -Y btatt -O hci_h4,btatt -r $1 | awk -F'[: ]+' '$1~/Frame/ { printf "\n%s ", $2}; $2~/Direct/ {printf "%s ", $3}; $2~/Value/ {printf "%s", $3}' > traffic
+
+#reverse traffic
+tac traffic > traffic_reversed
+rm -rf traffic
+
+#join fragments
+fragment_join=""
+while IFS="" read -r p || [ -n "$p" ];
+do
+	current_frame=$(echo "$p" | cut -d ' ' -f 3)
+	current_frame_start=$(echo $current_frame | cut -c 1,2)
+	if [ "$current_frame_start" != "aa" ]; then
+		fragment_join="$current_frame$fragment_join"
+	else
+		full_frame="$current_frame$fragment_join"
+		decod=$(./pySVS.py -d "$full_frame")
+		echo "$p$fragment_join $decod" >> traffic_reversed_decoded
+		echo "" >> traffic_reversed_decoded
+		fragment_join=""
+	fi
+done < traffic_reversed
+rm -rf traffic_reversed
+
+#reverse again
+tac traffic_reversed_decoded > traffic_decoded
+rm -rf traffic_reversed_decoded
+
+#show lines containing data
+cat traffic_decoded
+rm -rf traffic_decoded
+
+
+
+
 
